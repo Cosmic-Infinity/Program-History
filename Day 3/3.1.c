ada9270756083d9640ed999a36c5e9a787e3801a
Infinity (KIIT) <64971616+Cosmic-Infinity@users.noreply.github.com>
2024-08-23 12:49:42 +0530

    Update 3.1.c

--- Day 3/3.1.c
+++ Day 3/3.1.c
@@ -1,16 +1,19 @@
 // WAP to perform transpose of a given sparse matrix in 3-tuple format
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <limits.h>
 
 int main()
 {
-    int a[7][3], b[7][3], i, j, k = 1;
-    printf("Enter a 5x3 matrix in 3 tuple format : \n");
+    int a[6][3], b[6][3], i, j, k = 1;
+    printf("Enter a 6x3 matrix in 3 tuple format : \n");
     for (i = 0; i < 6; i++)
     {
         for (j = 0; j < 3; j++)
+        {
             scanf("%d", &a[i][j]);
+            b[i][j] = INT_MAX;
+        }
     }
 
     // printf("Taken data :\n");
@@ -18,7 +21,7 @@ int main()
     // for (i = 0; i <= a[0][2]; i++)
     // {
     //     for (j = 0; j < 3; j++)
-    //         printf("%d ", a[i][j]);
+    //         printf("%d ", b[i][j]);
     //     printf("\n");
     // }
 
@@ -33,25 +36,25 @@ int main()
                 b[k][2] = a[j][2];
                 k++;
             }
-            if (b[j][0] == b[j + 1][0])
+            if (b[j][0] == b[j + 1][0]) // possibly not memory safe because j+1 will read beyond index and j < a[0][2] condition cannot be used because the previous operation needs it
             {
                 if (b[j][1] > b[j + 1][1])
                 {
-                    int temp1 = b[j][0];
-                    int temp2 = b[j][1];
-                    int temp3 = b[j][2];
-                    b[j][0] = b[j + 1][0];
-                    b[j][1] = b[j + 1][1];
-                    b[j][2] = b[j + 1][2];
-                    b[j + 1][0] = temp1;
-                    b[j + 1][1] = temp2;
-                    b[j + 1][2] = temp3;
+                    b[j][0] = b[j][0] + b[j + 1][0];
+                    b[j][1] = b[j][1] + b[j + 1][1];
+                    b[j][2] = b[j][2] + b[j + 1][2];
+                    b[j + 1][0] = b[j][0] - b[j + 1][0];
+                    b[j + 1][1] = b[j][1] - b[j + 1][1];
+                    b[j + 1][2] = b[j][2] - b[j + 1][2];
+                    b[j][0] = b[j][0] - b[j + 1][0];
+                    b[j][1] = b[j][1] - b[j + 1][1];
+                    b[j][2] = b[j][2] - b[j + 1][2];
                 }
             }
         }
     }
 
-    // for (i = 0; i < a[0][2]; i++)
+    // for (i = 0; i < a[0][2]; i++) //this is safer a approch. to use another nested loop to perform the sorting of same columns.
     // {
     //     for (j = 1; j < a[0][2]; j++)
     //     {
